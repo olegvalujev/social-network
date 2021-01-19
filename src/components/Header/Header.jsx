369fba2ca4da8f8f273dd82d1a50206a143396d4
Oleg Valujev <oleg.valujev@gmail.com>
2021-01-19 15:59:55 +0200

    feature: add logout button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,9 @@ const Header = (props) => {
         <header className={ s.header }>
             <img src='https://cdn.pixabay.com/photo/2020/11/24/17/54/australian-shepherd-5773397_1280.jpg' alt='header'/>
             <div className={s.loginBlock}>
-                { props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink> }
+                { props.isAuth
+                    ? <div> {props.login} - <button onClick={props.logout}>Logout</button> </div>
+                    : <NavLink to={'/login'}>Login</NavLink> }
             </div>
         </header>
     )
