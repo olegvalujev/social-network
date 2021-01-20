2bbe7624cdf2437ee47da56f6aa4fc71fcf440d9
Oleg Valujev <oleg.valujev@gmail.com>
2021-01-20 14:54:01 +0200

    feature: add react-reselect

--- src/redux/UsersReducer.js
+++ src/redux/UsersReducer.js
@@ -81,15 +81,15 @@ export const setUsersTotalCount = (totalUsersCount) => ({type: SET_USERS_TOTAL_C
 export const toggleIsFetching = (isFetching) => ({type: TOGGLE_IS_FETCHING, isFetching})
 export const toggleFollowingInProgress = (isFetching, userId) => ({type: TOGGLE_IS_FOLLOWING_PROGRESS, isFetching, userId})
 
-export const requestUsers = (currentPage, pageSize) => (dispatch) => {
+export const requestUsers = (page, pageSize) => (dispatch) => {
     dispatch(toggleIsFetching(true))
-    usersAPI.getUsers(currentPage, pageSize)
-        .then(data => {
-                dispatch(toggleIsFetching(false))
-                dispatch(setUsers(data.items))
-                dispatch(setUsersTotalCount(data.totalCount))
-            }
-        )
+    dispatch(setCurrentPage(page))
+
+    usersAPI.getUsers(page, pageSize).then(data => {
+        dispatch(toggleIsFetching(false))
+        dispatch(setUsers(data.items))
+        dispatch(setUsersTotalCount(data.totalCount))
+    })
 }
 
 export const unFollow = (userId) => (dispatch) => {
