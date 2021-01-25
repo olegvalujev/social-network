960141083642d88ad074c85082d5c4b3a9074c93
Oleg Valujev <oleg.valujev@gmail.com>
2021-01-25 15:36:01 +0200

    style: User block contents position adjustment

--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -3,33 +3,36 @@ import styles from "./Users.module.css"
 import defaultAvatar from './../../assets/rudolf_small.png'
 import {NavLink} from "react-router-dom";
 
-const User = ({user,followingInProgress,unFollow,follow}) => {
+const User = ({user, followingInProgress, unFollow, follow}) => {
     return (
-            <div className={styles.userBlock}>
-                {user.status && <div>user.status</div>}
-                <div>
-                    <div>
-                        <NavLink to={'/profile/' + user.id}>
-                            <img src={user.photos.small !== null ? user.photos.small : defaultAvatar} className={styles.avatar}/>
-                        </NavLink>
-                    </div>
-                    <div>
-                        {
-                            user.followed
-                                ? <button disabled={followingInProgress.some(id => id === user.id)}
-                                    onClick={() => unFollow(user.id) }
-                                >Unfollow</button>
+        <div className={styles.userBlock}>
+            {user.status && <div>user.status</div>}
 
-                                : <button disabled={followingInProgress.some(id => id === user.id)}
-                                          onClick={() => follow(user.id)}>Follow</button>
-                        }
-                    </div>
-                </div>
+            <div className={styles.row}>
+                <NavLink to={'/profile/' + user.id}>
+                    <img src={user.photos.small !== null ? user.photos.small : defaultAvatar}
+                         className={styles.avatar}/>
+                </NavLink>
+            </div>
+
+            <div className={styles.row}>
+                <div><span className={styles.userDetails}>User name: </span>{user.name}</div>
+                <div><span className={styles.userDetails}>User status: </span>{user.status}</div>
                 <div>
-                    <div>{user.name}</div>
-                    <div>{user.status}</div>
+                {
+                    user.followed
+                        ? <button disabled={followingInProgress.some(id => id === user.id)}
+                                  onClick={() => unFollow(user.id) }
+                        >Unfollow</button>
+
+                        : <button disabled={followingInProgress.some(id => id === user.id)}
+                                  onClick={() => follow(user.id)}>Follow</button>
+                }
                 </div>
             </div>
+
+
+        </div>
     )
 }
 
