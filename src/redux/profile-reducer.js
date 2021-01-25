d9c0b700a1edbee8c84a28235cf5fa85f67a7be8
Oleg Valujev <oleg.valujev@gmail.com>
2021-01-25 14:54:12 +0200

    refactor: initial Proifle page state updated

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -9,13 +9,10 @@ export const SET_STATUS = 'SET_STATUS'
 
 let initialState = {
     posts: [
-        {id: 1, message: 'Domich', likesCount: 0},
-        {id: 2, message: 'Dima', likesCount: 0},
-        {id: 3, message: 'Dasha', likesCount: 0},
-        {id: 4, message: 'Pasha', likesCount: 0},
-        {id: 5, message: 'Katherina', likesCount: 0},
-        {id: 6, message: 'Dominika', likesCount: 0},
-        {id: 7, message: 'Maria', likesCount: 0},
+        {id: 1, message: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ', likesCount: 0},
+        {id: 2, message: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. ', likesCount: 0},
+        {id: 3, message: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.', likesCount: 0},
+        {id: 4, message: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?', likesCount: 0},
     ],
     profile: null,
     status: null,
