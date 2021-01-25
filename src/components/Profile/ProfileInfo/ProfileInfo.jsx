38264a9023104cff306c81e72f6661a08e74ad7b
Oleg Valujev <oleg.valujev@gmail.com>
2021-01-25 14:11:11 +0200

    refactor: Profile page default avatar image refactored

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,8 @@
-import s from './ProfileInfo.module.css';
+import styles from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
 import React from "react";
 import ProfileStatusWithHooks from './ProfileStatusWithHooks'
+import defaultAvatar from "../../../assets/snowman_medium.png";
 
 const ProfileInfo = ({profile, status, updateStatus}) => {
     if (!profile){
@@ -10,9 +11,9 @@ const ProfileInfo = ({profile, status, updateStatus}) => {
 
     return (
         <div>
-            <div className={s.descriptionBlock}>
+            <div className={styles.descriptionBlock}>
                 <div>
-                    <img src={profile.photos.large}/>
+                    <img src={profile.photos.large !== null ? profile.photos.large : defaultAvatar} className={styles.avatar}/>
                 </div>
                 <ProfileStatusWithHooks
                     status={status}
@@ -24,13 +25,13 @@ const ProfileInfo = ({profile, status, updateStatus}) => {
                 </div>
                 <div>
                     <ul>
-                        <li>Facebook: {profile.contacts.facebook}</li>
-                        <li>Website: {profile.contacts.facebook}</li>
-                        <li>VK: {profile.contacts.facebook}</li>
-                        <li>Twitter: {profile.contacts.facebook}</li>
-                        <li>Instagram: {profile.contacts.facebook}</li>
-                        <li>YouTube: {profile.contacts.facebook}</li>
-                        <li>GitHub: {profile.contactsfacebook}</li>
+                        {profile.contacts.facebook && <li>Facebook: {profile.contacts.facebook}</li>}
+                        {profile.contacts.website && <li>Website: {profile.contacts.website}</li>}
+                        {profile.contacts.vk && <li>VK: {profile.contacts.vk}</li>}
+                        {profile.contacts.twitter && <li>Twitter: {profile.contacts.twitter}</li>}
+                        {profile.contacts.instagram && <li>Instagram: {profile.contacts.instagram}</li>}
+                        {profile.contacts.youtube && <li>YouTube: {profile.contacts.youtube}</li>}
+                        {profile.contacts.github && <li>GitHub: {profile.contacts.github}</li>}
                     </ul>
                 </div>
                 { profile.lookingForAJob
