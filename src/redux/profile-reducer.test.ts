609f9cba8c1797cd48b0225c6b666576ca5cfbbc
olegvalujev <oleg.valujev@gmail.com>
2021-02-17 15:28:48 +0200

    refactor: profile reducer test types added

--- src/redux/profile-reducer.test.ts
+++ src/redux/profile-reducer.test.ts
@@ -1,12 +1,31 @@
 import profileReducer, {actions} from "./profile-reducer";
+import {PostType, ProfileType} from "../types/types";
 
 let initialState = {
     posts: [
-        {id: 1, message: 'Domich', likesCount: 0},
-        {id: 2, message: 'Dima', likesCount: 0},
-        {id: 3, message: 'Dasha', likesCount: 0},
-        {id: 4, message: 'Pasha', likesCount: 0},
-    ]
+        {
+            id: 1,
+            message: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ',
+            likesCount: 0
+        },
+        {
+            id: 2,
+            message: 'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. ',
+            likesCount: 0
+        },
+        {
+            id: 3,
+            message: 'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.',
+            likesCount: 0
+        },
+        {
+            id: 4,
+            message: 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?',
+            likesCount: 0
+        },
+    ] as Array<PostType>,
+    profile: null as ProfileType | null,
+    status: "",
 }
 
 it('New post should be added', () => {
